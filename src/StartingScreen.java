770c058555321031c014e8f7db0ac64cf802d2d6
Hyeong Kyun (Daniel) Park <park1119@purdue.edu>
2022-02-19 16:38:00 -0500

    a

---
+++ src/StartingScreen.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class StartingScreen {
+    JFrame f;
+    StartingScreen() {
+        f = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+        f.add(b);//adding button in JFrame  
+        f.setSize(400,500);//400 width and 500 height  
+        f.setLayout(null);//using no layout managers  
+        f.setVisible(true);//making the frame visible 
+    }
+}
